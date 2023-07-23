30247c4cadc24d2fc3debb0ef05c7f1e032d4571
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2023-07-23 13:07:02 -0300

    OCP

---
+++ OCP/Program.cs
@@ -0,0 +1,24 @@
+﻿using OCP.Vehicles;
+using System;
+
+namespace OCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeVehicle type = TypeVehicle.CAR;
+
+            if (type == TypeVehicle.CAR) 
+            {
+                Car vehicle = new Car("Vermelho", 2022, 1.8, 5, 4);
+            }
+            else
+            {
+                Motorcycle vehicle = new Motorcycle("Branco", 2023, 250);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
