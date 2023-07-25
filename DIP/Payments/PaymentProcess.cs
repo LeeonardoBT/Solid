094bff66746fd6094aa974a51deb4443cf91760c
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2023-07-24 21:29:32 -0300

    DIP

---
+++ DIP/Payments/PaymentProcess.cs
@@ -0,0 +1,16 @@
+﻿using DIP.Factory;
+using DIP.Model;
+using System;
+
+namespace DIP.Payments
+{
+    class PaymentProcess
+    {
+        public void Pay(string id)
+        {
+            DbProduct product = DbProductFactory.Create();
+            string productData = product.GetProductById(id);
+            Console.WriteLine(productData);
+        }
+    }
+}
